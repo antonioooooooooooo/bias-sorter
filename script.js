089d0f4060acaa693c4b29eb5a3e37bff2902f1b
Anton Schukin <antonschukin@gmail.com>
2022-11-09 23:03:37 +0200

    fix

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
 
-  while (currentIndex != 0) {
+  while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
